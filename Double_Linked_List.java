c1dec283c1c34f0fda86999fad8b85bf2dfbd4dc
Anil Kumar Battula <anilkumarbattula1120@gmail.com>
2024-10-25 00:21:58 +0530

    Code is added

--- Double_Linked_List.java
+++ Double_Linked_List.java
@@ -41,6 +41,35 @@ public class Double_Linked_List {
         tail = temp;
 
     }
+    //To delete node at First
+    public void removeFirst() {
+        if(head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        if(head == tail) {
+            head = tail = null;
+        }
+        else{
+        head = head.next;
+        head.prev = null;
+        }
+
+    }
+    //To delete node at end
+    public void removeEnd() {
+        if(head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        if(head == tail) {
+            head = tail = null;
+        }
+        else{
+        tail = tail.prev;
+        tail.next = null;
+        }
+    }
     //To print the linked list
     public void printList() {
         if(head == null){
@@ -57,8 +86,13 @@ public class Double_Linked_List {
 
     public static void main(String args[]) {
         Double_Linked_List list = new Double_Linked_List();
+        list.addFirst(3);
+        list.addFirst(2);
         list.addFirst(1);
         list.addFirst(0);
+        list.printList();
+        list.removeFirst();
+        list.removeEnd();
         list.printList();
     }
 }
