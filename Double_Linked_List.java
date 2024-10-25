11425ce7f774f05b8cd489148c1cc40e9c684d5e
Anil Kumar Battula <anilkumarbattula1120@gmail.com>
2024-10-26 00:55:46 +0530

    code added

--- Double_Linked_List.java
+++ Double_Linked_List.java
@@ -41,6 +41,39 @@ public class Double_Linked_List {
         tail = temp;
 
     }
+    //Insert at a Particular Position
+    public void addPosition(int idx, int data) {
+        Node temp = new Node(data);
+        if(head == null) {
+            head = tail = temp;
+            return;
+        }
+        if(idx == 0) {
+            temp.next = head;
+            head.prev = temp;
+            head = temp;
+            return;
+        }
+        Node curr = head;
+        for(int i =0; i < idx-1 && curr != null; i++) {
+            curr = curr.next;
+        }
+        if(curr == null) {
+            System.out.println("Index out of Bound");
+            return;
+        }
+        if(curr.next == null) {
+            curr.next = temp;
+            temp.prev = curr;
+            temp.next = null;
+            tail = temp;
+            return;
+        }
+        temp.next = curr.next;
+        temp.next.prev = temp;
+        curr.next = temp;
+        temp.prev = curr;
+    }
     //To delete node at First
     public void removeFirst() {
         if(head == null) {
@@ -70,6 +103,38 @@ public class Double_Linked_List {
         tail.next = null;
         }
     }
+    //To delete node at Particular Position
+    public void removePosition(int idx) {
+        if(head == null) {
+            System.out.println("List is Empty");
+            return;
+        }
+        if(idx == 0) {
+            System.out.println(head.data + " is removed");
+            head = head.next;
+            head.prev = null;
+            return;
+        }
+        Node curr = head;
+        for(int i = 0; i < idx-1 && curr != null; i++ ) {
+            curr = curr.next;
+        }
+        if(curr == null) {
+            System.out.println("Index out of bound");
+        }
+        if(curr.next == null) {
+            System.out.println(tail.data + " is removed");
+            tail.prev = tail;
+            tail.next = null;
+        }
+        else{
+            System.out.println(curr.next.data + " is removed");
+            curr.next.prev = null;
+            curr.next.next.prev = curr;
+            curr.next = curr.next.next;
+        }
+
+    }
     //To print the linked list
     public void printList() {
         if(head == null){
@@ -92,8 +157,16 @@ public class Double_Linked_List {
         list.addFirst(0);
         list.printList();
         list.removeFirst();
-        list.removeEnd();
+        list.removeEnd(); 
         list.printList();
+        list.addPosition(2,7);
+        list.addPosition(1,14);
+        list.printList();
+        list.removePosition(1);
+        list.removePosition(3);
+        list.printList();
+
+
     }
 }
 
